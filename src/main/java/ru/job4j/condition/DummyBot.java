8edf235a01149c8d0edd47ef465819ab83d68eff
EkaterinaLom <Lomakinaei@mail.ru>
2022-03-14 19:50:28 +1000

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -5,11 +5,9 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-            rsl = "Привет умник";
+            rsl = "Привет, умник.";
         } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
-        } else if ("Сколько будет 2 + 2?".equals(question)) {
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rsl;
     }
